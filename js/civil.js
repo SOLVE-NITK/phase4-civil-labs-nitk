5db9b805ebe79479d26da987884d9cc484b284b2
SOLVE-NITK <61772077+SOLVE-NITK@users.noreply.github.com>
2024-06-07 16:33:45 +0530

    Update civil.js

--- js/civil.js
+++ js/civil.js
@@ -47,63 +47,63 @@ let labs = [
       },
     ],
   },
-  {
-    labName: "Geo Technical Engineering Lab",
-    experiments: [
-      {
-        id: "Standard Proctor test",
-        path: "geo_technical/exp-standard-proctor-test",
-      },
-      {
-        id: "Liquid limit and plastic limit",
-        path: "geo_technical/exp-liquid-limit-and-plastic-limit",
-      },
-      {
-        id: "Dry density by core cutter method",
-        path: "geo_technical/exp-standard-proctor-test",
-      },
-    ],
-  },
-  {
-    labName: "Ground Water Engineering Lab",
-    experiments: [
-      {
-        id: "Georesistivity test",
-        path: "ground_water/Georesistivity-test-nitk",
-      },
-      {
-        id: "Percolation rate test",
-        path: "ground_water/Percolation-rate-test-nitk",
-      },
-      {
-        id: "Determination of the Transmissivity of an Aquifer Soil",
-        path: "ground_water/Transmissivity-of-an-aquifer",
-      },
-    ],
-  },
-  {
-    labName: "Remote Sensing and GIS Lab",
-    experiments: [
-      { id: "Cartography", path: "remote_sensing/exp-cartography" },
-      { id: "Geo Reference", path: "remote_sensing/exp-geo-reference" },
-      { id: "Leaf Area Index", path: "remote_sensing/exp-leaf-area-index" },
-    ],
-  },
+  // {
+  //   labName: "Geo Technical Engineering Lab",
+  //   experiments: [
+  //     {
+  //       id: "Standard Proctor test",
+  //       path: "geo_technical/exp-standard-proctor-test",
+  //     },
+  //     {
+  //       id: "Liquid limit and plastic limit",
+  //       path: "geo_technical/exp-liquid-limit-and-plastic-limit",
+  //     },
+  //     {
+  //       id: "Dry density by core cutter method",
+  //       path: "geo_technical/exp-standard-proctor-test",
+  //     },
+  //   ],
+  // },
+  // {
+  //   labName: "Ground Water Engineering Lab",
+  //   experiments: [
+  //     {
+  //       id: "Georesistivity test",
+  //       path: "ground_water/Georesistivity-test-nitk",
+  //     },
+  //     {
+  //       id: "Percolation rate test",
+  //       path: "ground_water/Percolation-rate-test-nitk",
+  //     },
+  //     {
+  //       id: "Determination of the Transmissivity of an Aquifer Soil",
+  //       path: "ground_water/Transmissivity-of-an-aquifer",
+  //     },
+  //   ],
+  // },
+  // {
+  //   labName: "Remote Sensing and GIS Lab",
+  //   experiments: [
+  //     { id: "Cartography", path: "remote_sensing/exp-cartography" },
+  //     { id: "Geo Reference", path: "remote_sensing/exp-geo-reference" },
+  //     { id: "Leaf Area Index", path: "remote_sensing/exp-leaf-area-index" },
+  //   ],
+  // },
   {
     labName: "Physical Metallurgy Lab",
     experiments: [
-      {
-        id: "Microstructural analysis of stainless steels",
-        path: "http://vlabs.iitb.ac.in/vlabs-dev/labs/nitk_labs/physical-metallurgy/labs/microstructural-analysis-nitk/index.html",
-      },
-      {
-        id: "Comparison between mild steel and grey cast iron",
-        path: "http://vlabs.iitb.ac.in/vlabs-dev/labs/nitk_labs/physical-metallurgy/labs/mildsteel-castiron-nitk/index.html",
-      },
-      {
-        id: "Severity of quenching",
-        path: "http://vlabs.iitb.ac.in/vlabs-dev/labs/nitk_labs/physical-metallurgy/labs/quenching-severity-nitk/index.html",
-      },
+      // {
+      //   id: "Microstructural analysis of stainless steels",
+      //   path: "http://vlabs.iitb.ac.in/vlabs-dev/labs/nitk_labs/physical-metallurgy/labs/microstructural-analysis-nitk/index.html",
+      // },
+      // {
+      //   id: "Comparison between mild steel and grey cast iron",
+      //   path: "http://vlabs.iitb.ac.in/vlabs-dev/labs/nitk_labs/physical-metallurgy/labs/mildsteel-castiron-nitk/index.html",
+      // },
+      // {
+      //   id: "Severity of quenching",
+      //   path: "http://vlabs.iitb.ac.in/vlabs-dev/labs/nitk_labs/physical-metallurgy/labs/quenching-severity-nitk/index.html",
+      // },
       {
         id: "Grain Size Measurement",
         path: "physicalMetallurgy/exp-grain-size-measuremnt/index.html",
